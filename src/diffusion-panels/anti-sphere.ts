f98ca2590ad28be1d2398bd08a1fda34ff5bcb74
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-19 19:12:33 -0400

    📝 (anti-sphere.ts): Update panel width, height, and thickness for better aesthetics and functionality 📝 (anti-sphere.ts): Increase sphere radius, spacing, and add raise property for improved visualization and spacing 📝 (anti-sphere.ts): Adjust number of segments for smoother rendering and enhanced geometry consistency

--- src/diffusion-panels/anti-sphere.ts
+++ src/diffusion-panels/anti-sphere.ts
@@ -3,38 +3,50 @@ import { subtract, union } from "@jscad/modeling/src/operations/booleans";
 import { expand } from "@jscad/modeling/src/operations/expansions";
 import { translate } from "@jscad/modeling/src/operations/transforms";
 import { cuboid, sphere, square } from "@jscad/modeling/src/primitives";
+import { count } from "console";
 
 const isEven = (n: number) => n % 2 === 0;
 
 const panel = {
-  width: 100,
-  height: 100,
-  thickness: 10,
+  width: 180,
+  height: 180,
+  thickness: 20,
 };
 
 const spheres = {
-  radius: 20,
+  radius: 40,
   phase: 0.5,
-  spacing: 20,
+  spacing: 60,
+  raise: {
+    x: 10,
+    y: 10,
+  },
 };
 
-const segments = 10;
+const segments = 100;
 
 // A function declaration that returns geometry
 export const main = () => {
   const phaseSpacing = spheres.spacing * spheres.phase;
-  const spheresGeo = [
-    ...[...Array(panel.width / spheres.spacing + 1).keys()],
-  ].map((i) => [
+  const widthCount = panel.width / spheres.spacing;
+  const heightCount = panel.height / spheres.spacing;
+
+  const spheresGeo = [...[...Array(widthCount + 1).keys()]].map((i) => [
     translate(
       [
         panel.width / 2 - i * spheres.spacing,
         isEven(i) ? phaseSpacing / 2 : -phaseSpacing / 2,
-        0,
+        spheres.raise.x * -Math.abs(widthCount / 2 - i) + widthCount + 1 / 2,
       ],
-      ...[...Array(panel.height / spheres.spacing + 1).keys()].map((j) =>
+      ...[...Array(heightCount + 1).keys()].map((j) =>
         translate(
-          [0, panel.height / 2 - j * spheres.spacing, 0],
+          [
+            0,
+            panel.height / 2 - j * spheres.spacing,
+            spheres.raise.y * -Math.abs(heightCount / 2 - j) +
+              heightCount +
+              1 / 2,
+          ],
           sphere({ radius: spheres.radius, segments })
         )
       )
@@ -42,9 +54,27 @@ export const main = () => {
   ]);
 
   return subtract(
-    cuboid({
-      size: [panel.width, panel.height, panel.thickness * 2],
-    }),
-    translate([0, 0, spheres.radius], ...spheresGeo.flat())
+    union(
+      cuboid({
+        size: [panel.width, panel.height, panel.thickness],
+        center: [0, 0, -panel.thickness / 2],
+      }),
+      cuboid({
+        size: [
+          panel.width,
+          panel.height,
+          spheres.radius + Math.max(spheres.raise.x, spheres.raise.y),
+        ],
+        center: [
+          0,
+          0,
+          (spheres.radius + Math.max(spheres.raise.x, spheres.raise.y)) / 2,
+        ],
+      })
+    ),
+    translate(
+      [0, 0, spheres.radius + 2 * Math.max(spheres.raise.x, spheres.raise.y)],
+      ...spheresGeo.flat()
+    )
   );
 };
