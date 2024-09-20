e70b606e1f6842282709fa691e85aba8b594b25c
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-19 20:35:31 -0400

    ✨ (bracket.ts): introduce the 'width' property to BeamProps interface for specifying the width of beams   ♻️ (bracket.ts): refactor the 'panel' function to use the 'cuboid' primitive instead of 'square' for better geometry definition   📝 (bracket.ts): add comments to clarify the purpose and parameters of the 'panel' and 'beam' functions

--- src/diffusion-panels/bracket.ts
+++ src/diffusion-panels/bracket.ts
@@ -1,5 +1,6 @@
+import { subtract } from "@jscad/modeling/src/operations/booleans";
 import { expand } from "@jscad/modeling/src/operations/expansions";
-import { cube, square } from "@jscad/modeling/src/primitives";
+import { cube, cuboid, square } from "@jscad/modeling/src/primitives";
 
 enum Part {
   Panel,
@@ -13,14 +14,31 @@ interface PanelProps {
 
 interface BeamProps {
   length: number;
+  width: number;
   thickness: number;
 }
 
 const part: Part = Part.Panel;
 
-const panel = ({}: PanelProps) => {};
+const panel = ({ width, beamThickness }: PanelProps) => {
+  return subtract(
+    cuboid({
+      size: [width, width, beamThickness],
+    }),
+    cuboid({
+      size: [
+        width - beamThickness / 4,
+        width - beamThickness / 4,
+        beamThickness,
+      ],
+      center: [beamThickness / 8, beamThickness / 8, beamThickness / 4],
+    })
+  );
+};
 
-const beam = ({}: BeamProps) => {};
+const beam = ({}: BeamProps) => {
+  return cube({ size: 10 });
+};
 
 // A function declaration that returns geometry
 export const main = () => {
@@ -33,6 +51,7 @@ export const main = () => {
     case Part.Beam:
       return beam({
         length: 180,
+        width: 20,
         thickness: 10,
       });
   }
