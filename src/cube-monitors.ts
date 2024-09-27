926f54f1c47e03cbdf6ae5d72aaece8ce676f591
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-09-26 23:23:32 -0400

    ✨ (cube-monitors.ts): Add support for a new 'speaker' component to create a speaker geometry in the 3D model using the 'cylinder' primitive. ♻️ (cube-monitors.ts): Refactor the code structure to improve readability and maintainability by extracting geometry creation functions for the 'shell' and 'speaker' components.

--- src/cube-monitors.ts
+++ src/cube-monitors.ts
@@ -1,5 +1,10 @@
-import { subtract } from "@jscad/modeling/src/operations/booleans";
-import { cuboid, roundedCuboid } from "@jscad/modeling/src/primitives";
+import { subtract, union } from "@jscad/modeling/src/operations/booleans";
+import {
+  cuboid,
+  cylinder,
+  roundedCuboid,
+} from "@jscad/modeling/src/primitives";
+import convert from "convert";
 
 const shell = {
   width: 180,
@@ -7,19 +12,31 @@ const shell = {
   radius: 10,
 };
 
+const speaker = {
+  diameter: convert(4, "in").to("mm"),
+};
+
 export const main = () => {
-  return subtract(
-    roundedCuboid({
-      size: [shell.width, shell.width, shell.width],
-      roundRadius: shell.radius,
-    }),
-    cuboid({
-      size: [
-        shell.width - shell.thickness * 2,
-        shell.width - shell.thickness * 2,
-        shell.width - shell.thickness,
-      ],
-      center: [0, 0, shell.thickness],
-    })
-  );
+  const shellGeo = ({ width, radius, thickness }: typeof shell) => {
+    return subtract(
+      roundedCuboid({
+        size: [width, width, width],
+        roundRadius: radius,
+      }),
+      cuboid({
+        size: [width - thickness * 2, width - thickness * 2, width],
+        center: [0, 0, thickness],
+      })
+    );
+  };
+
+  const speakerGeo = ({ diameter }: typeof speaker) => {
+    return cylinder({
+      radius: diameter / 2,
+      height: shell.thickness,
+      center: [0, 0, -(shell.width - shell.thickness) / 2],
+    });
+  };
+
+  return subtract(shellGeo(shell), speakerGeo(speaker));
 };
