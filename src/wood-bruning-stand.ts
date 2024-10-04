aff6975f9c80e378ff3adbfc1b5393cd7dfef8bb
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-10-04 19:58:31 -0400

    🐛 (wood-burning-stand.ts): Adjust roundRadius value from 4 to 2 for correct calculations in the wood holder structure.   ♻️ (wood-burning-stand.ts): Refactor the wood holder geometry to properly expand and union the shapes for better visualization and accuracy.

--- src/wood-bruning-stand.ts
+++ src/wood-bruning-stand.ts
@@ -7,18 +7,18 @@ import {
   roundedCuboid,
 } from "@jscad/modeling/src/primitives";
 
-const roundRadius = 4;
+const roundRadius = 2;
 const segments = 10;
 
 const candleHolder = {
   height: 50,
   depth: 30,
   thickness: 20,
-  diameter: 50 + roundRadius,
+  diameter: 50 + roundRadius * 2,
 };
 
 const woodHolder = {
-  thickness: 5,
+  thickness: 6 - roundRadius * 2,
   diameter: 20 + roundRadius,
   height: 50,
   tray: {
@@ -30,7 +30,7 @@ const woodHolder = {
 export const main = () => {
   const candleHolderGeo = () => {
     return expand(
-      { delta: roundRadius, corners: "round", segments },
+      { delta: roundRadius, segments },
       subtract(
         cylinder({
           radius: candleHolder.diameter / 2 + candleHolder.thickness,
@@ -48,34 +48,37 @@ export const main = () => {
   };
 
   const woodHolderGeo = () => {
-    return union(
-      subtract(
-        cylinder({
-          radius: woodHolder.diameter / 2 + woodHolder.thickness,
-          height: woodHolder.height,
-          center: [0, 0, woodHolder.height / 2],
-          segments,
-        }),
-        cylinder({
-          radius: (woodHolder.diameter - woodHolder.thickness / 2) / 2,
-          height: woodHolder.height - woodHolder.thickness,
-          center: [0, 0, (woodHolder.height + woodHolder.thickness) / 2],
-          segments,
-        })
-      ),
-      subtract(
-        cylinder({
-          radius: woodHolder.tray.diameter / 2 + woodHolder.thickness,
-          height: woodHolder.tray.height,
-          center: [0, 0, woodHolder.tray.height / 2],
-          segments,
-        }),
-        cylinder({
-          radius: woodHolder.tray.diameter / 2,
-          height: woodHolder.tray.height - woodHolder.thickness,
-          center: [0, 0, (woodHolder.tray.height + woodHolder.thickness) / 2],
-          segments,
-        })
+    return expand(
+      { delta: roundRadius, segments },
+      union(
+        subtract(
+          cylinder({
+            radius: woodHolder.diameter / 2 + woodHolder.thickness,
+            height: woodHolder.height,
+            center: [0, 0, woodHolder.height / 2],
+            segments,
+          }),
+          cylinder({
+            radius: (woodHolder.diameter - woodHolder.thickness / 2) / 2,
+            height: woodHolder.height - woodHolder.thickness,
+            center: [0, 0, (woodHolder.height + woodHolder.thickness) / 2],
+            segments,
+          })
+        ),
+        subtract(
+          cylinder({
+            radius: woodHolder.tray.diameter / 2 + woodHolder.thickness,
+            height: woodHolder.tray.height,
+            center: [0, 0, woodHolder.tray.height / 2],
+            segments,
+          }),
+          cylinder({
+            radius: woodHolder.tray.diameter / 2,
+            height: woodHolder.tray.height - woodHolder.thickness,
+            center: [0, 0, (woodHolder.tray.height + woodHolder.thickness) / 2],
+            segments,
+          })
+        )
       )
     );
   };
