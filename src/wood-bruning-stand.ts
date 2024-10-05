4602724c2f8f78188f60b97770c2fada32babac3
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-10-04 21:52:18 -0400

    ♻️ (wood-bruning-stand.ts): Update the segments variable value from 20 to 10 for better geometry precision and performance. 📝 (wood-bruning-stand.ts): Refactor the woodHolderGeo function to simplify cylinder calculations and improve code readability. Add depth property to woodHolder object for better tray depth control.

--- src/wood-bruning-stand.ts
+++ src/wood-bruning-stand.ts
@@ -7,7 +7,7 @@ const PHI = 1.618033988749895;
 const baseHeight = 25;
 
 const roundRadius = 2;
-const segments = 20;
+const segments = 10;
 
 const candleHolder = {
   height: baseHeight * PHI,
@@ -20,6 +20,7 @@ const woodHolder = {
   thickness: 6 - roundRadius * 2,
   diameter: 20 + roundRadius * 2,
   height: candleHolder.height * PHI,
+  depth: 40,
   tray: {
     height: baseHeight / PHI - roundRadius,
     diameter: 2 * ((candleHolder.diameter / 2 + candleHolder.thickness) * PHI),
@@ -48,39 +49,36 @@ export const main = () => {
   };
 
   const woodHolderGeo = () => {
-    return expand(
-      { delta: roundRadius, segments },
-      union(
-        subtract(
-          cylinder({
-            radius: woodHolder.diameter / 2 + woodHolder.thickness,
-            height: woodHolder.height,
-            center: [0, 0, woodHolder.height / 2],
-            segments,
-          }),
-          cylinder({
-            radius: (woodHolder.diameter - woodHolder.thickness / 2) / 2,
-            height: woodHolder.height - woodHolder.thickness,
-            center: [0, 0, (woodHolder.height + woodHolder.thickness) / 2],
-            segments,
-          })
-        ),
-        subtract(
-          cylinder({
-            radius: woodHolder.tray.diameter / 2 + woodHolder.thickness,
-            height: woodHolder.tray.height,
-            center: [0, 0, woodHolder.tray.height / 2],
-            segments,
-          }),
-          cylinder({
-            radius: woodHolder.tray.diameter / 2,
-            height: woodHolder.tray.height - woodHolder.thickness,
-            center: [0, 0, (woodHolder.tray.height + woodHolder.thickness) / 2],
-            segments,
-          })
-        )
-      )
+    const stand = subtract(
+      cylinder({
+        radius: woodHolder.diameter / 2 + woodHolder.thickness,
+        height: woodHolder.height,
+        center: [0, 0, woodHolder.height / 2],
+        segments,
+      }),
+      cylinder({
+        radius: (woodHolder.diameter - woodHolder.thickness / 2) / 2,
+        height: woodHolder.depth,
+        center: [0, 0, woodHolder.height - woodHolder.depth / 2],
+        segments,
+      })
     );
+    const tray = subtract(
+      cylinder({
+        radius: woodHolder.tray.diameter / 2 + woodHolder.thickness,
+        height: woodHolder.tray.height,
+        center: [0, 0, woodHolder.tray.height / 2],
+        segments,
+      }),
+      cylinder({
+        radius: woodHolder.tray.diameter / 2,
+        height: woodHolder.tray.height - woodHolder.thickness,
+        center: [0, 0, (woodHolder.tray.height + woodHolder.thickness) / 2],
+        segments,
+      })
+    );
+
+    return expand({ delta: roundRadius, segments }, union(stand, tray));
   };
 
   const offset =
