15a93ec1799260acd948dfbed609ba6a77f276ca
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-10-10 19:34:48 -0400

    ♻️ (helpers.ts): refactor the map function to directly return an array of type T using Array.from method for better readability and performance.

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,9 @@ import { Geom2, Geom3 } from "@jscad/modeling/src/geometries/types";
 
 export const map = <T extends Geom2 | Geom3>(
   count: number,
-  callback: (value: number, index: number, array: number[]) => T
-): Array<T> => {
-  return [...Array(count).keys()].map(callback) as Array<T>;
+  callback: (value: number, index: number, array: number[]) => T // Use T directly
+): T[] => {
+  return Array.from({ length: count }, (_, index) =>
+    callback(index, index, Array.from({ length: count }))
+  );
 };
