d110c1492bb19058cbe1f16a4d42c16c2a24086e
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-10-17 12:07:58 -0400

    ♻️ (coat-rack.ts): Simplify import statements by removing duplicate 'union' import and formatting code for better readability   🐛 (coat-rack.ts): Fix calculation error in leg geometry where height was incorrectly computed as twice the actual height   🐛 (coat-rack.ts): Fix calculation error in screw geometry where the dimensions were incorrectly defined for the cylindrical and elliptical shapes

--- src/coat-rack.ts
+++ src/coat-rack.ts
@@ -3,12 +3,7 @@ import convert from "convert";
 import { map } from "./helpers";
 import { cylinder, cylinderElliptic } from "@jscad/modeling/src/primitives";
 import { rotate, translate } from "@jscad/modeling/src/operations/transforms";
-import {
-  subtract,
-  union,
-  union,
-  union,
-} from "@jscad/modeling/src/operations/booleans";
+import { subtract, union } from "@jscad/modeling/src/operations/booleans";
 import { hull } from "@jscad/modeling/src/operations/hulls";
 
 const TAU = Math.PI * 2;
@@ -33,26 +28,8 @@ const rack = {
   },
 };
 
-export const main = () => {
-  const frameGeo = hull(
-    map(rack.legs.count, (i) => {
-      return rotate(
-        [0, 0, i * (TAU / rack.legs.count)],
-        translate(
-          [rack.radius, 0, 0],
-          rotate(
-            [rack.angle, 0, 0],
-            cylinder({
-              radius: rack.legs.diameter / 2 + rack.thickness,
-              height: rack.height,
-            })
-          )
-        )
-      );
-    })
-  );
-
-  const legGeo = map(rack.legs.count, (i) => {
+const frameGeo = hull(
+  map(rack.legs.count, (i) => {
     return rotate(
       [0, 0, i * (TAU / rack.legs.count)],
       translate(
@@ -60,62 +37,80 @@ export const main = () => {
         rotate(
           [rack.angle, 0, 0],
           cylinder({
-            radius: rack.legs.diameter / 2,
-            height: rack.height * 2,
+            radius: rack.legs.diameter / 2 + rack.thickness,
+            height: rack.height,
           })
         )
       )
     );
-  });
+  })
+);
 
-  const screwGeo = map(rack.legs.count, (i) => {
-    return rotate(
-      [0, 0, i * (TAU / rack.legs.count)],
-      translate(
-        [rack.radius, 0, 0],
-        rotate(
-          [rack.angle, 0, 0],
-          union(
-            rotate(
-              [0, Math.PI / 2, 0],
-              map(rack.screws.count, (i) =>
-                translate(
-                  [
-                    i * (rack.screws.spread / rack.screws.count) -
-                      rack.screws.spread / 4,
-                    0,
-                    (rack.legs.diameter + rack.thickness) / 2,
-                  ],
-                  union(
-                    cylinder({
-                      height: rack.thickness * 2,
-                      radius: rack.screws.diameter / 2,
-                    }),
-                    cylinderElliptic({
-                      height: rack.screws.head.height,
-                      startRadius: [
-                        rack.screws.diameter / 2,
-                        rack.screws.diameter / 2,
-                      ],
-                      endRadius: [
-                        rack.screws.head.diameter / 2,
-                        rack.screws.head.diameter / 2,
-                      ],
-                      center: [
-                        0,
-                        0,
-                        (rack.thickness - rack.screws.head.height) / 2,
-                      ],
-                    })
-                  )
+const legGeo = map(rack.legs.count, (i) => {
+  return rotate(
+    [0, 0, i * (TAU / rack.legs.count)],
+    translate(
+      [rack.radius, 0, 0],
+      rotate(
+        [rack.angle, 0, 0],
+        cylinder({
+          radius: rack.legs.diameter / 2,
+          height: rack.height * 2,
+        })
+      )
+    )
+  );
+});
+
+const screwGeo = map(rack.legs.count, (i) => {
+  return rotate(
+    [0, 0, i * (TAU / rack.legs.count)],
+    translate(
+      [rack.radius, 0, 0],
+      rotate(
+        [rack.angle, 0, 0],
+        union(
+          rotate(
+            [0, Math.PI / 2, 0],
+            map(rack.screws.count, (i) =>
+              translate(
+                [
+                  i * (rack.screws.spread / rack.screws.count) -
+                    rack.screws.spread / 4,
+                  0,
+                  (rack.legs.diameter + rack.thickness) / 2,
+                ],
+                union(
+                  cylinder({
+                    height: rack.thickness * 2,
+                    radius: rack.screws.diameter / 2,
+                  }),
+                  cylinderElliptic({
+                    height: rack.screws.head.height,
+                    startRadius: [
+                      rack.screws.diameter / 2,
+                      rack.screws.diameter / 2,
+                    ],
+                    endRadius: [
+                      rack.screws.head.diameter / 2,
+                      rack.screws.head.diameter / 2,
+                    ],
+                    center: [
+                      0,
+                      0,
+                      (rack.thickness - rack.screws.head.height) / 2,
+                    ],
+                  })
                 )
               )
             )
           )
         )
       )
-    );
-  });
+    )
+  );
+});
 
+export const main = () => {
   return subtract(frameGeo, legGeo, screwGeo);
 };
